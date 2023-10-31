425cdb2104538e179d5ff67f8764c44ab36d5ebd
DistantLight <dist4ntlight@gmail.com>
2023-10-31 16:15:24 +0300

    init commit

---
+++ src/main/java/com/distantlight/CharFrequencyCalculation/CharFrequencyCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.distantlight.CharFrequencyCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharFrequencyCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharFrequencyCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distantlight/CharFrequencyCalculation/CharFrequencyCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distantlight.CharFrequencyCalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharFrequencyCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
